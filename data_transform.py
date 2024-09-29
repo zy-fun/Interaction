f98315a81500cf4881c2e4ed27d90b7c9c7c9b9f
zy-fun <2020zhouyi@sjtu.edu.cn>
2024-09-29 14:56:09 +0800

    modify fetch_data_from_xml(debug)

--- data_transform.py
+++ data_transform.py
@@ -1,26 +1,46 @@
 from lxml import etree
 import numpy as np
 import csv
+import os
 
 # fetch roadnet data from osm.net.xml file
 def fetch_data_from_xml(xml_file, target_dir):
+    # create edge_dict from xml file
     tree = etree.parse(xml_file)
     root = tree.getroot()
     
-    edges = root.xpath('//edge')
-    edge2attr = {}
+    # edges = root.xpath('//edge')
+    edges = root.xpath('//edge[@from and @to]')
+    edge_dict = {}
     for edge in edges:
-        id = edge.get('id')
+        edge_id = edge.get('id')
         shape = edge.xpath('./lane/@shape')[0]
         lengths = [float(i) for i in edge.xpath('./lane/@length')]
         length = str(np.average(lengths))
         from_node_id = edge.get('from')
         to_node_id = edge.get('to')
-        edge2attr[id] = {
+        edge_dict[edge_id] = {
+            'edge_id': edge_id,
+            'from_node_id': from_node_id,
+            'to_node_id': to_node_id,
             'shape': shape,
             'length': length
         }
-    return edge2attr
+
+    # edges = root.xpath('//edge[@from and @to]')
+    # for node in nodes:
+
+    
+    # write edge_dict to csv file
+    csv_file = os.path.join(target_dir, 'edge.csv')
+    with open(csv_file, 'w') as f:
+        field_names = ['edge_id', 'from_node_id', 'to_node_id', 'shape', 'length']
+        dict_writer = csv.DictWriter(f, field_names)
+        dict_writer.writeheader()
+        for _, edge in edge_dict.items():
+            dict_writer.writerow(edge)
+    
+    # construct adj
 
 if __name__ == "__main__":
     ret = fetch_data_from_xml('data/osm.net.xml', 'data')
