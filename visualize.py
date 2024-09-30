ba44a3a611a2ba55ccaf3d3d2d38109e3cd487d5
zy-fun <2020zhouyi@sjtu.edu.cn>
2024-09-30 10:10:03 +0800

    update visualize

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import csv
+import numpy as np
 # import pandas as pd
 
 def plot_roadnet(edge_file):
@@ -8,9 +9,53 @@ def plot_roadnet(edge_file):
         for row in reader:
             shape = [[float(i) for i in coord.split(',')] for coord in row['shape'].split(' ')]
             x, y = list(zip(*shape))
-            plt.plot(x, y)
+            plt.plot(x, y, color='black')
     plt.savefig('roadnet.png')
 
+def plot_roadnet_with_eccentric_road(edge_file, short_threshold=0.75, long_threshold=2):
+    with open(edge_file, 'r') as f:
+        reader = csv.DictReader(f)
+        lengths = []
+        for row in reader:
+            lengths.append(float(row['length']))
+        lengths = np.array(lengths)
+        avg, std = np.mean(lengths), np.std(lengths)
+        print('avg: ', avg)
+        print('std: ', std)
+
+    with open(edge_file, 'r') as f:
+        reader = csv.DictReader(f)
+        overlong_count = 0
+        overshort_count = 0
+        for row in reader:
+            shape = [[float(i) for i in coord.split(',')] for coord in row['shape'].split(' ')]
+            x, y = list(zip(*shape))
+            if float(row['length']) > avg + long_threshold * std:
+                plt.plot(x, y, color='red', zorder=10)
+                overlong_count += 1
+            elif float(row['length']) < avg - short_threshold * std:
+                plt.plot(x, y, color='blue', zorder=11)
+                overshort_count += 1
+            else:
+                plt.plot(x, y, color='black', alpha=0.2)
+                pass
+    
+        plt.plot([], [], color='red', label=f"overlong: {overlong_count}")
+        plt.plot([], [], color='blue', label=f"overshort: {overshort_count}")
+        plt.legend()
+    plt.savefig('roadnet.png')
+
+def plot_road_legnth_hist(edge_file):
+    with open(edge_file, 'r') as f:
+        reader = csv.DictReader(f)
+        lengths = []
+        for row in reader:
+            lengths.append(float(row['length']))
+    plt.hist(lengths, bins=100)
+    plt.savefig('road_length_hist.png')
+
 if __name__ == '__main__':
-    plot_roadnet('data/edge.csv')
+    # plot_roadnet('data/edge.csv')
+    plot_roadnet_with_eccentric_road('data/edge.csv', short_threshold=0.5)
+    # plot_road_legnth_hist('data/edge.csv')
     pass
