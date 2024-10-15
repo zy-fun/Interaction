3b7c3d8b7e5d5a3efc771382684f12e6342460e7
zy-fun <2020zhouyi@sjtu.edu.cn>
2024-10-15 16:48:21 +0800

    timepred init

---
+++ timepred.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Block(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, output_dim)
+        self.fc2 = nn.Linear(output_dim, output_dim)
+        self.fc = nn.Linear(input_dim, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x  
+
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+
+        out += self.fc(identity)
+        out = self.relu(out)
+        return out 
+
+class TimePredModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        pass
+
+    def forward(self, loc_state, traffic_state, time_state):
+        '''
+        input:
+            loc_state: (batch_size, seq_len, loc_dim)
+            traffic_state: (batch_size, seq_len, traffic_dim)
+            time_state: (batch_size, seq_len, time_dim)
+        '''
+        x = torch.cat([loc_state, traffic_state, time_state], dim=-1)   # (batch_size, seq_len, feature_in)
+        B, T, C = x.size()
+
+        x = x.reshape(B, T * C)
+        x = self.fc1(x)
+        pass
+
+if __name__ == "__main__":
+    x = torch.randn(2, 3, 4)
+    block = Block(4, 5)
+    out = block(x)
