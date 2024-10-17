ea6af4feddb35b4e05748e5163def90141262e4d
zy-fun <2020zhouyi@sjtu.edu.cn>
2024-10-17 19:19:21 +0800

    complete __init__() of TimePredModel

--- timepred.py
+++ timepred.py
@@ -25,28 +25,60 @@ class Block(nn.Module):
         out = self.relu(out)
         return out 
 
-class TimePredModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        pass
+# class RouteEncoder(nn.Module):
+#     def __init__(self, 
+#                  input_dim: int = 3,
+#                  hidden_dim: int = 16,):
+#         super().__init__()
+#         self.fc1 = nn.Linear(input_dim, hidden_dim)
+#         self.relu1 = nn.ReLU()
+#         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+#         self.relu2 = nn.ReLU()
 
-    def forward(self, loc_state, traffic_state, time_state):
+#     def forward(self, x):
+#         out = self.fc1(x)
+#         out = self.relu(out)
+#         out = self.fc2(out)
+#         out = self.relu(out)
+#         return out
+
+class TimePredModel(nn.Module):
+    def __init__(
+            self, 
+            route_dim: int = 3, 
+            space_dim: int = 1, 
+            time_dim: int = 1, 
+            route_hidden: int = 16,
+            state_hidden: int = 16,
+            window_size: int = 3, 
+            block_dims: list = [], 
+        ):
+        super().__init__()
+        self.route_encoder = Block(route_dim * window_size, route_hidden)
+        self.state_encoder = Block(space_dim * window_size + time_dim, state_hidden)
+        block_dims = [route_hidden + state_hidden] + block_dims
+        block_dims = zip(block_dims[:-1], block_dims[1:])
+        self.blocks = nn.ModuleList([Block(in_dim, out_dim) for in_dim, out_dim in block_dims])
+        self.sigmoid = nn.Sigmoid() 
+
+    def forward(self, route, space_state, time_state):
         '''
         input:
-            loc_state: (batch_size, seq_len, loc_dim)
-            traffic_state: (batch_size, seq_len, traffic_dim)
-            time_state: (batch_size, seq_len, time_dim)
+            route: (batch_size, window_size, route_dim)
+            space_state: (batch_size, window_size, space_dim)
+            time_state: (batch_size, time_dim)
         '''
-        x = torch.cat([loc_state, traffic_state, time_state], dim=-1)   # (batch_size, seq_len, feature_in)
-        B, T, C = x.size()
+        B, W, _ = route.size()
 
-        x = x.reshape(B, T * C)
-        x = self.fc1(x)
-        pass
+        route = route.reshape(B, -1)
+        route = self.route_encoder(route)
+
+        space_state = space_state.reshape(B, -1)
+        state = torch.cat([space_state, time_state], dim=-1)
+        state = self.state_encoder(state)
+
+        out = torch.cat([route, state], dim=-1)
+        return out   
 
 if __name__ == "__main__":
-    x = torch.randn(2, 3, 4)
-    block = Block(4, 5)
-    out = block(x)
-    print(out.shape)
+    pass
