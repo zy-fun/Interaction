390a7e601bdddad16df5b5643f7eb0c674315683
zy-fun <2386756261@qq.com>
2024-10-21 11:20:14 +0800

    add plot_traj(debugging)

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import csv
 import numpy as np
 from lxml import etree
+import ast
 # import pandas as pd
 
 def plot_roadnet(edge_file):
@@ -65,18 +66,34 @@ def plot_roadnet_from_xml(edges_file, nodes_file, fig_save_path):
     plt.figure(figsize=(16, 16))
     edges_tree = etree.parse(edges_file)
     edges = edges_tree.getroot()
+    edges_loc = {}
     for edge in edges.xpath('./edge'):
         from_id = edge.get('from')
         to_id = edge.get('to')
         x, y = list(zip(node_loc[from_id], node_loc[to_id]))
+        edges_loc[edge.get('id')] = x, y
         plt.plot(x, y, color='black', alpha=.5)
         
     plt.savefig(fig_save_path)
+    plt.close()
+    return plt.gcf(), edges_loc
 
+def plot_traj(fig, edges_loc, traj_file, fig_save_path):
+    plt.figure(fig)
+    with open(traj_file, 'r') as trajs:
+        for traj in trajs:
+            id_seq = set(ast.literal_eval(traj_point)[1] for traj_point in traj.split()[1:])
+            loc_seq = [edges_loc.get(str(id), -9999) for id in id_seq]
+            print(loc_seq)
+            break
+    # plt.plot(edges_loc['1'][0], edges_loc['1'][1], color='red')
+    plt.savefig(fig_save_path)
 
 if __name__ == '__main__':
     # plot_roadnet('data/edge.csv')
     # plot_roadnet_with_eccentric_road('data/edge.csv', short_threshold=0.5)
     # plot_road_legnth_hist('data/edge.csv')
-    plot_roadnet_from_xml('data/shenzhen/edge_sumo.edg.xml', 'data/shenzhen/node_sumo.nod.xml', 'fig/shenzhen/roadnet.png')
+    # roadnet_fig, edges_loc = None, {}
+    roadnet_fig, edges_loc = plot_roadnet_from_xml('data/shenzhen/edge_sumo.edg.xml', 'data/shenzhen/node_sumo.nod.xml', 'fig/shenzhen/roadnet.png')
+    plot_traj(roadnet_fig, edges_loc, 'data/shenzhen/shenzhen_test_traj.txt', 'fig/shenzhen/traj.png')
     pass
