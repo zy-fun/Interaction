e31a1a929c9b2a29f233a621b7c5f76e301cba09
zy-fun <2386756261@qq.com>
2024-10-25 15:26:37 +0800

    can visualize continuous trajs now

--- visualize.py
+++ visualize.py
@@ -56,7 +56,7 @@ def plot_road_legnth_hist(edge_file):
     plt.hist(lengths, bins=100)
     plt.savefig('road_length_hist.png')
 
-def plot_roadnet_from_xml(edges_file, nodes_file, fig_save_path):
+def plot_roadnet_from_xml(edges_file, nodes_file, fig_save_path, mapping_dict=None):
     nodes_tree = etree.parse(nodes_file)
     nodes = nodes_tree.getroot()
     node_loc = {
@@ -71,20 +71,38 @@ def plot_roadnet_from_xml(edges_file, nodes_file, fig_save_path):
         from_id = edge.get('from')
         to_id = edge.get('to')
         x, y = list(zip(node_loc[from_id], node_loc[to_id]))
-        edges_loc[edge.get('id')] = x, y
-        plt.plot(x, y, color='black', alpha=.5)
+        # index = mapping_dict[edge.get('id')] if mapping_dict else int(edge.get('id'))
+        index = int(edge.get('id'))
+        edges_loc[index] = x, y
+        plt.plot(x, y, color='black', alpha=.1)
         
     plt.savefig(fig_save_path)
-    plt.close()
     return plt.gcf(), edges_loc
+
+# def get_mapping_dict_from_csv(edge_mapping_file):
+#     '''
+#         mapping 'Edge ID' (which is used in edge_sumo.edg.xml)
+#         to 'Index' (which is used in traj files. e.g. shenzhen_test_traj.txt)
+#     '''
+#     import pandas as pd
+#     df = pd.read_csv(edge_mapping_file)
+#     mapping_dict = dict(zip(df['Edge ID'], df['Index']))
+#     return mapping_dict
 
 def plot_traj(fig, edges_loc, traj_file, fig_save_path):
     plt.figure(fig)
     with open(traj_file, 'r') as trajs:
-        for traj in trajs:
-            id_seq = set(ast.literal_eval(traj_point)[1] for traj_point in traj.split()[1:])
-            loc_seq = [edges_loc.get(str(id), -9999) for id in id_seq]
+        # for traj in list(trajs)[:2]:
+        while True:
+            traj = list(trajs)[1]
+            id_seq = [ast.literal_eval(traj_point)[1] for traj_point in traj.split()[1:]]
+            id_seq = [id_seq[i]+1 for i in range(len(id_seq)) if i == 0 or id_seq[i] != id_seq[i - 1]]
+            print(id_seq)
+            loc_seq = [edges_loc.get(id, -9999) for id in id_seq]
             print(loc_seq)
+            for loc in loc_seq:
+                if loc != -9999:
+                    plt.plot(loc[0], loc[1], linewidth=3, color='red', zorder=100)
             break
     # plt.plot(edges_loc['1'][0], edges_loc['1'][1], color='red')
     plt.savefig(fig_save_path)
@@ -93,7 +111,6 @@ if __name__ == '__main__':
     # plot_roadnet('data/edge.csv')
     # plot_roadnet_with_eccentric_road('data/edge.csv', short_threshold=0.5)
     # plot_road_legnth_hist('data/edge.csv')
-    # roadnet_fig, edges_loc = None, {}
-    roadnet_fig, edges_loc = plot_roadnet_from_xml('data/shenzhen/edge_sumo.edg.xml', 'data/shenzhen/node_sumo.nod.xml', 'fig/shenzhen/roadnet.png')
-    plot_traj(roadnet_fig, edges_loc, 'data/shenzhen/shenzhen_test_traj.txt', 'fig/shenzhen/traj.png')
+    roadnet_fig, edges_loc = plot_roadnet_from_xml('data/shenzhen_8_6/edge_sumo.edg.xml', 'data/shenzhen_8_6/node_sumo.nod.xml', 'fig/shenzhen_8_6/roadnet.png')
+    plot_traj(roadnet_fig, edges_loc, 'data/shenzhen_8_6/shenzhen_test_traj.txt', 'fig/shenzhen_8_6/traj.png')
     pass
