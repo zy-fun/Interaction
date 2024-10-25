ab61fa3bbb056a9dafafe10ba17221039eb599a9
zy-fun <2386756261@qq.com>
2024-10-25 16:24:10 +0800

    add get_model

--- util.py
+++ util.py
@@ -1,21 +1,13 @@
-from PIL import Image
-import os
+from timepred import TimePredModel
 
-# 图片文件夹路径
-image_folder = 'gnnfig'
-
-# 获取图片文件列表
-images = [os.path.join(image_folder, img) for img in os.listdir(image_folder)]
-
-# 打开所有图片并添加到列表中
-frames = []
-for image in images:
-    new_frame = Image.open(image)
-    frames.append(new_frame)
-
-# 保存为 GIF 动图
-frames[0].save(os.path.join(image_folder, 'gnn.gif'), format='GIF',
-               append_images=frames[1:],
-               save_all=True,
-               duration=100,  # 每帧的显示时间，单位为毫秒
-               loop=0)       # 循环次数，0 表示无限循环
+def get_model(cfg):
+    model = TimePredModel(
+        route_dim=cfg.route_dim, 
+        space_dim=cfg.space_dim, 
+        time_dim=cfg.time_dim, 
+        route_hidden=cfg.route_hidden, 
+        state_hidden=cfg.state_hidden, 
+        window_size=cfg.window_size, 
+        block_dims=cfg.block_dims
+    ) 
+    return model
