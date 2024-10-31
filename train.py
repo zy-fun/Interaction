f550c27a293ea159896280002ca944bcf15c68ca
zy-fun <2386756261@qq.com>
2024-10-31 15:55:50 +0800

    debugging

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     # model define
     parser.add_argument('--vocab_size', type=int, default=27910, help='vocab size')
-    parser.add_argument('--window_size', type=int, default=3, help='window size')
-    parser.add_argument('--route_dim', type=int, default=3, help='route dimension')
+    parser.add_argument('--window_size', type=int, default=2, help='window size')
+    parser.add_argument('--route_dim', type=int, default=1, help='route dimension')
     parser.add_argument('--space_dim', type=int, default=1, help='space dimension')
     parser.add_argument('--time_dim', type=int, default=1, help='time dimension')
     parser.add_argument('--route_hidden', type=int, default=16, help='route hidden dimension')
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     parser.add_argument('--block_dims', type=int, nargs='+', default=[512, 256, 128, 64, 32, 16, 1], help='block dimensions')
 
     # optimization
+    parser.add_argument('--device', type=str, default='cuda', help='device')
     parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
     parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=10, help='early stopping patience')
@@ -38,19 +39,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     train_data = get_dataloader(args, data_type='test')
-    val_data = get_dataloader(args, data_type='val')
-    model = get_model(args)
-    criterion = nn.BCEWithLogitsLoss()
+    # val_data = get_dataloader(args, data_type='val')
+    device = torch.device(args.device)
+    model = get_model(args).to(device)
+    criterion = nn.BCEWithLogitsLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
 
     window_size = args.window_size
-
     for epoch in range(args.train_epochs):
         train_loss = []
         for i, (batch_x, batch_y) in tqdm(enumerate(train_data)):
-            route_id, route, space_state, time_state = batch_x[:, :window_size], batch_x[:, window_size:window_size*2], batch_x[:, window_size*2:window_size*2+1], batch_x[:, window_size*2+1:-1]
-            current_time = batch_x[:, -1]
-            out = model(route, space_state, time_state, route_id)
+        # while True:
+            # print(batch_x.shape)
+            route_id, route, space_state, time_state = batch_x[:, :window_size], batch_x[:, window_size:window_size*2], batch_x[:, window_size*2:window_size*3], batch_x[:, window_size*3:-1]
+            route_id = route_id.to(device, torch.int64)
+            route = route.to(device)
+            space_state = space_state.to(device)
+            time_state = time_state.to(device)
+            batch_y = batch_y.to(torch.float32).to(device)
+            # route_id = torch.randint(0, args.vocab_size, (args.batch_size, window_size)).to(device)
+            # space_state = torch.randn(args.batch_size, window_size, args.space_dim).to(device)
+            # time_state = torch.randn(args.batch_size, args.time_dim).to(device)
+            # route = torch.randn(args.batch_size, window_size, args.route_dim).to(device)
+            # batch_y = torch.randint(0, 2, (args.batch_size, 1)).to(torch.float32).to(device)
+
+            print(route_id.shape, route.shape, space_state.shape, time_state.shape)
+            print(route_id[0], route[0], space_state[0], time_state[0])
+            out = model(route, space_state, time_state, route_id).squeeze()         
+
             loss = criterion(out, batch_y)
             optimizer.zero_grad()
             loss.backward()
@@ -66,4 +82,3 @@ if __name__ == "__main__":
             #         loss = criterion(out, val_batch_y)
             #     print(f'Epoch {epoch} Iter {i} Train Loss {sum(train_loss) / len(train_loss)} Val Loss {sum(val_loss) / len(val_loss)}')
             #     train_loss = []
-    
