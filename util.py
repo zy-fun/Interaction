ff402bc005bc2cf9016c11943693575d6a61dae6
zy-fun <2386756261@qq.com>
2024-11-04 16:05:26 +0800

    update get_model

--- util.py
+++ util.py
@@ -1,14 +1,21 @@
-from timepred import TimePredModel
+from timepredmodel import TimePredModel, MLPModel
 
 def get_model(cfg):
-    model = TimePredModel(
-        vocab_size=cfg.vocab_size,
-        route_dim=cfg.route_dim, 
-        space_dim=cfg.space_dim, 
-        time_dim=cfg.time_dim, 
-        route_hidden=cfg.route_hidden, 
-        state_hidden=cfg.state_hidden, 
-        window_size=cfg.window_size, 
-        block_dims=cfg.block_dims
-    ) 
+    if cfg.name == 'mlp':
+        model = MLPModel(
+            input_dim=cfg.input_dim, 
+            hidden_dims=cfg.hidden_dims, 
+            output_dim=cfg.output_dim
+        )
+    else:
+        model = TimePredModel(
+            vocab_size=cfg.vocab_size,
+            route_dim=cfg.route_dim, 
+            space_dim=cfg.space_dim, 
+            time_dim=cfg.time_dim, 
+            route_hidden=cfg.route_hidden, 
+            state_hidden=cfg.state_hidden, 
+            window_size=cfg.window_size, 
+            block_dims=cfg.block_dims
+        ) 
     return model
