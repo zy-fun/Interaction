0aa4f9b90d3d973835c0776e825da4c6cf65081c
zy-fun <2386756261@qq.com>
2024-11-05 14:22:57 +0800

    add minidataset(debugging)

--- data_provider/dataset.py
+++ data_provider/dataset.py
@@ -7,6 +7,86 @@ import numpy as np
 import subprocess
 from lxml import etree
 
+class miniDataset(Dataset):
+    def __init__(self, root_path, flag, window_size=2, traj_num=1000, time_unit=5):
+        assert flag in ['train', 'val', 'test']
+        self.window_size = window_size
+        self.flag = flag
+        self.root_path = root_path
+        self.traj_num = traj_num
+        self.time_unit = time_unit
+
+        self.x_data = []    # [route_id(window_size), route_feature(window_size), space_state(window_size), time_stay, curr_time]
+        self.y_data = []    # T or F
+        self.__read_original_data(root_path)
+
+    def __read_original_data(self, root_path):
+        # unit time is not defined well and may cause error
+        path_dict = {
+            'train': os.path.join(root_path, f"shenzhen_train_traj.txt"),
+            'val': os.path.join(root_path, f"shenzhen_val_traj.txt"),
+            'test': os.path.join(root_path, f"shenzhen_test_traj.txt"),
+            'edge': os.path.join(root_path, f"edge_sumo.edg.xml"),
+        }
+
+        # calculate the total traffic at each time and location
+        traffic_dict = {}   # (time, loc) -> traffic
+        with open(path_dict[self.flag], 'r') as f:
+            for line in list(f)[:self.traj_num]:
+                trajs = [ast.literal_eval(traj_point) for traj_point in line.split()[1:]]
+                for t, loc in trajs:
+                    if (t, loc) in traffic_dict:
+                        traffic_dict[(t, loc)] += 1
+                    else:
+                        traffic_dict[(t, loc)] = 1
+
+        # normalize the traffic dict
+        for key in traffic_dict:
+            traffic_dict[key] /= self.traj_num / 1e4  
+
+        # create edge_features from file
+        edge_features = {}
+        edges_tree = etree.parse(path_dict['edge'])
+        edges = edges_tree.getroot()
+        for edge in edges.xpath('./edge'):
+            edge_id = int(edge.get('id')) - 1
+            edge_features[edge_id] = float(edge.get('length'))
+
+        # create x, y from file
+        with open(path_dict[self.flag], 'r') as f:
+            for line in list(f)[:self.traj_num]:
+                time, duplicate_loc = zip(*[ast.literal_eval(traj_point) for traj_point in line.split()[1:]])
+                time = np.array(time) // self.time_unit
+                duplicate_loc = np.array(duplicate_loc)
+                change_indices = np.append([0], np.where(np.diff(duplicate_loc) != 0)[0] + 1)
+                route = list(duplicate_loc[change_indices]) + [0] * (self.window_size - 1)
+                route_context_dict = {route[i]:route[i:i+self.window_size] for i in range(len(route) - self.window_size + 1)}
+                stay_time = [0] * len(duplicate_loc)
+                for i in range(len(duplicate_loc)):
+                    if i == 0 or duplicate_loc[i] != duplicate_loc[i-1]:
+                        pass
+                    else:
+                        stay_time[i] = stay_time[i-1] + 1
+                a_x_data = [route_context_dict[edge].copy() for edge in duplicate_loc]
+                for i in range(len(a_x_data)):
+                    route_context = a_x_data[i]
+                    route_feature = [float(edge_features.get(edge, 0)) for edge in route_context]
+                    space_state = [traffic_dict.get((time[i], edge), 0) for edge in route_context]
+                    time_stay = [stay_time[i]]
+                    curr_time = [time[i]]
+                    a_x_data[i] += route_feature + space_state + time_stay + curr_time
+                
+                self.x_data += a_x_data
+                a_y_data = (duplicate_loc != np.append(duplicate_loc[1:], 0)).tolist()
+                self.y_data += a_y_data
+
+    def __len__(self):
+        return len(self.y_data)
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.x_data[idx]), torch.tensor(self.y_data[idx])
+
+
 class Dataset_Shenzhen(Dataset):
     def __init__(self, root_path, flag, window_size=2, time_range=['00:00:00', '23:59:59'], time_unit=5, ):
         assert flag in ['train', 'val', 'test']
@@ -68,7 +148,6 @@ class Dataset_Shenzhen(Dataset):
             for line in f:
                 time, duplicate_loc = zip(*[ast.literal_eval(traj_point) for traj_point in line.split()[1:]])
                 time = np.array(time) // self.time_unit
-                traj_start_time = time[0]
                 duplicate_loc = np.array(duplicate_loc)
                 change_indices = np.append([0], np.where(np.diff(duplicate_loc) != 0)[0] + 1)
                 route = list(duplicate_loc[change_indices]) + [0] * (self.window_size - 1)
@@ -104,4 +183,5 @@ if __name__ == "__main__":
     for batch in data_loader:
         x, y = batch
         print(x, y)
+        print(x.shape, y.shape)
         break 
