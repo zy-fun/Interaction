e67e0eb6ecb55d45fb2e67d280b2b2c0c48b1f8c
zy-fun <2386756261@qq.com>
2024-11-14 21:03:59 +0800

    update api

--- data_provider/data_provider.py
+++ data_provider/data_provider.py
@@ -1,5 +1,5 @@
 from torch.utils.data import DataLoader
-from data_provider.dataset import Dataset_Shenzhen, miniDataset, DownSampledDataset
+from data_provider.dataset import Dataset_Shenzhen, miniDataset, DownSampledDataset, BinaryClassificationDataset
 
 def get_dataloader(cfg, data_type: str):
     data_name = cfg.data_name
@@ -7,12 +7,14 @@ def get_dataloader(cfg, data_type: str):
         'shenzhen_8_6': 'data_provider/data/shenzhen_8_6',
         'mini_data': 'data_provider/data/shenzhen_8_6'
     }
-    root_path = root_path_dict[data_name]
+    root_path = root_path_dict.get(data_name, None)
  
     if data_name == 'mini_data':
         data = miniDataset(root_path, flag=data_type, window_size=cfg.window_size)
     elif data_name == 'shenzhen_8_6':
         data = Dataset_Shenzhen(root_path=root_path, flag=data_type, window_size=cfg.window_size)
+    elif data_name == 'binary_classification':
+        data = BinaryClassificationDataset(n_features=cfg.input_dim, data_type=data_type)
 
     if cfg.downsample:
         data = DownSampledDataset(data.x_data, data.y_data)
