23052e9603427dfcdc9fdf346faf10a513755d13
zy-fun <2386756261@qq.com>
2024-11-18 20:11:37 +0800

    add pretrain.py

---
+++ data_provider/pretrain.py
@@ -0,0 +1,33 @@
+import dask.bag as db
+import re
+import time
+import pickle
+
+def read_shenzhen_txt_db(filename):
+    bag = db.read_text(filename, blocksize='32MB')
+    print(bag.npartitions)
+    results = bag.map(lambda line: [int(i) for i in re.findall(r'\d+', line)[1:]]).compute()
+    return results
+
+def shenzhen_txt_to_pkl():
+    for data_type in ['train', 'val', 'test']:
+        data_type = 'test'
+        file_path = f"data_provider/data/shenzhen_8_6/shenzhen_{data_type}_traj.txt"
+        start_time  = time.time()
+        results = read_shenzhen_txt_db(file_path)
+        print("Time: ", time.time() - start_time)
+
+        save_path = f'data_provider/data/shenzhen_8_6/shenzhen_{data_type}_traj.pkl'
+        with open(save_path, 'wb') as file:
+            pickle.dump(results, file) 
+
+if __name__ == "__main__":
+    data_type = 'val'
+    file_path = f"data_provider/data/shenzhen_8_6/shenzhen_{data_type}_traj.txt"
+    start_time  = time.time()
+    results = read_shenzhen_txt_db(file_path)
+    print("Time: ", time.time() - start_time)
+
+    save_path = f'data_provider/data/shenzhen_8_6/shenzhen_{data_type}_traj.pkl'
+    with open(save_path, 'wb') as file:
+        pickle.dump(results, file) 
